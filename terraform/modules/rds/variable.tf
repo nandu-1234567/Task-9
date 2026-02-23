08973f8720be6437a3e659b9383dae3c0bdb2c2a
Balla Hima Nandini <himanandiniballa2017@gmail.com>
2026-02-23 17:36:09 +0530

    Update variable.tf

--- terraform/modules/rds/variable.tf
+++ terraform/modules/rds/variable.tf
@@ -1,21 +1,31 @@
 variable "project_name" {}
+
 variable "db_username" {
-  type        = string
   description = "Master username for the RDS instance"
+  type        = string
+  default     = "admin"  # permanent default if not supplied
 }
 
 variable "db_password" {
-  type        = string
   description = "Master password for the RDS instance"
+  type        = string
   sensitive   = true
+  default     = "SuperSecret123!"  # default password for testing, override in prod
 }
 
 variable "db_name" {
-  type        = string
   description = "Database name"
+  type        = string
+  default     = "strapi_db"
 }
-variable "db_instance_class" {}
-variable "subnets" {
-  type = list(string)
+
+variable "db_instance_class" {
+  description = "RDS instance class"
+  type        = string
+  default     = "db.t3.micro"
 }
-variable "security_group_id" {}
+
+variable "security_group_id" {
+  description = "Security group for RDS"
+  type        = string
+}
